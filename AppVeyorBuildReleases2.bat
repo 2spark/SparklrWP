d15d4f970b06d409df0ac4c004d6bdba46479829
Marocco2 <marocco2@live.it>
2014-08-17 13:42:13 +0200

    test8

--- AppVeyorBuildReleases2.bat
+++ AppVeyorBuildReleases2.bat
@@ -1,2 +1,2 @@
-curl -XPOST -H "Authorization:token 6540826d8a3b35ee133e148a1b2000502fd90e07" -H "Content-Type:application/octet-stream" -H "Content-Length:%size%" --data-binary @SparklrForWindowsPhone.xap https://uploads.github.com/repos/2spark/SparklrWP/releases/%s/assets?name=SparklrForWindowsPhone.xap
+curl -XPOST -H "Authorization:token 6540826d8a3b35ee133e148a1b2000502fd90e07" -H "Content-Type:application/octet-stream" -H "Content-Length:%size%" --data-binary @SparklrForWindowsPhone.xap https://uploads.github.com/repos/2spark/SparklrWP/releases/%PLATFORM%_%APPVEYOR_BUILD_VERSION%/assets?name=SparklrForWindowsPhone.xap
 EXIT
