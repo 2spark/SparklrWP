f4d82c3a48b4825c7e0dda3f147b17e4c25b7de2
Marocco2 <marocco2@live.it>
2014-08-17 12:09:52 +0200

    test6

--- AppVeyorBuildReleases.bat
+++ AppVeyorBuildReleases.bat
@@ -6,5 +6,5 @@ del json.json
 move c:\projects\SparklrWP\SparklrForWindowsPhone\SparklrForWindowsPhone\Bin\%PLATFORM%\%CONFIGURATION%\SparklrForWindowsPhone_%CONFIGURATION%_%PLATFORM%.xap c:\projects\SparklrWP
 rename c:\projects\SparklrWP\SparklrForWindowsPhone_%CONFIGURATION%_%PLATFORM%.xap SparklrForWindowsPhone.xap
 file_size.bat "c:\projects\SparklrWP\SparklrForWindowsPhone.xap"
-curl -H "Authorization:token 6540826d8a3b35ee133e148a1b2000502fd90e07" -H "Content-Type:application/octet-stream" -H "Content-Length:%size%" --data-binary @SparklrForWindowsPhone.xap https://uploads.github.com/repos/2spark/SparklrWP/releases/%s/assets?name=SparklrForWindowsPhone.xap
+curl -XPOST -H "Authorization:token 6540826d8a3b35ee133e148a1b2000502fd90e07" -H "Content-Type:application/octet-stream" -H "Content-Length:%size%" --data-binary @SparklrForWindowsPhone.xap https://uploads.github.com/repos/2spark/SparklrWP/releases/%s/assets?name=SparklrForWindowsPhone.xap
 EXIT
